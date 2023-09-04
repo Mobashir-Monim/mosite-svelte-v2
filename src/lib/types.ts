143bc0663b5318478c9d4cd4a82937d510566abf
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-05 03:22:33 +0600

    Add additional types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,18 +1,17 @@
 export interface UIFileType {
 	type: 'file';
 	name: string;
-	icon: ConstructorOfATypedSvelteComponent;
 }
 
 export interface UIFolderType {
 	type: 'folder';
 	name: string;
-	contents: (UIFileType | UIFolderType)[];
+	contents: UIFileOrFolderType[];
 }
 
 export interface DirectoryStateType {
 	name: string;
-	contents: (UIFileType | UIFolderType)[];
+	contents: UIFileOrFolderType[];
 	size: number;
 	left: number;
 	top: number;
@@ -21,4 +20,11 @@ export interface DirectoryStateType {
 export interface IconPropsType {
 	size?: number;
 	classes?: string;
+}
+
+export type UIFileOrFolderType = UIFileType | UIFolderType;
+
+export interface RangeTimeType {
+	month: string;
+	year: string;
 }
