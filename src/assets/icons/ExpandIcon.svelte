043065c676915a714e04429530068c697cafca2a
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-06 22:37:53 +0600

    Add icons for window buttons

---
+++ src/assets/icons/ExpandIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let size: number = 48;
+	export let classes: string = 'fill-gray-600 dark:fill-gray-200';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	class="transit {classes}"
+	height={size}
+	width={size}
+>
+	<path
+		d="m480-236 93-93q12-12 29-12t29 12q12 12 12 29t-12 29L508-148q-6 6-13 8.5t-15 2.5q-8 0-15-2.5t-13-8.5L329-271q-12-12-12-29t12-29q12-12 29-12t29 12l93 93Zm0-484-93 93q-12 12-29 12t-29-12q-12-12-12-29t12-29l123-123q6-6 13-8.5t15-2.5q8 0 15 2.5t13 8.5l123 123q12 12 12 29t-12 29q-12 12-29 12t-29-12l-93-93Z"
+	/>
+</svg>
---
+++ src/assets/icons/MinimizeIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let size: number = 48;
+	export let classes: string = 'fill-gray-600 dark:fill-gray-200';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	class="transit {classes}"
+	height={size}
+	width={size}
+>
+	<path
+		d="M280-120q-17 0-28.5-11.5T240-160q0-17 11.5-28.5T280-200h400q17 0 28.5 11.5T720-160q0 17-11.5 28.5T680-120H280Z"
+	/>
+</svg>
