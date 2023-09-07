db4f29c74d233578b2ae577fe637e503a5f73947
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-08 05:55:23 +0600

    Add icons for folder traversal

--- src/components/WindowComponents/WindowToolBar.svelte
+++ src/components/WindowComponents/WindowToolBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { closeWindow } from '$lib/store/global-directory-system-store-control';
+	import ChevronIcon from '../../assets/icons/ChevronIcon.svelte';
 	import WindowToolBarButton from './WindowToolBarButton.svelte';
 
 	export let windowName: string;
@@ -18,7 +19,21 @@
 	class="cursor-move w-full h-[30px] bg-surface-500 flex flex-row justify-between py-[2.5px] px-[10px]"
 	tabindex="0"
 >
-	<h3 class="text-[0.6rem] font-normal">{windowName}</h3>
+	<div class="flex flex-row gap-2">
+		<div class="flex flex-row gap-0">
+			<ChevronIcon
+				size={20}
+				classes="my-auto fill-gray-600 dark:fill-gray-200 cursor-pointer"
+				direction="left"
+			/>
+			<ChevronIcon
+				size={20}
+				classes="my-auto fill-gray-600 dark:fill-gray-200 cursor-pointer"
+				direction="right"
+			/>
+		</div>
+		<h3 class="text-[0.6rem] font-normal">{windowName}</h3>
+	</div>
 	<div class="flex flex-row gap-[5px] my-auto">
 		<WindowToolBarButton buttonType="full-screen" />
 		<WindowToolBarButton buttonType="minimize" />
