cdc2e7c74ae49937b3af87693f26bf5b9f27dde9
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-08 01:59:29 +0600

    Update toolbar to show files and folder when one is open

--- src/components/ToolBarComponents/ToolBar.svelte
+++ src/components/ToolBarComponents/ToolBar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { globalDirectorySystemStore } from '$lib/store';
+	import type { WindowStateType } from '$lib/types';
 	import AppsIcon from '../../assets/icons/AppsIcon.svelte';
 	import OpenDocumentIcon from '../../assets/icons/OpenDocumentIcon.svelte';
 	import OpenFolderIcon from '../../assets/icons/OpenFolderIcon.svelte';
@@ -7,14 +9,29 @@
 	import ToolBarContentDivider from './ToolBarContentDivider.svelte';
 
 	const toolBarIconSize: number = 35;
+	let windows: WindowStateType[];
+	globalDirectorySystemStore.subscribe((value) => {
+		windows = value;
+	});
+
+	$: hasOpenFile = windows.filter((w) => w.type === 'file').length > 0;
+	$: hasOpenFolder =
+		windows.filter((w) => w.type === 'folder' && w.name !== 'root' && w.name !== 'settings')
+			.length > 0;
 </script>
 
 <div class="absolute bottom-0 w-full flex flex-row justify-center">
 	<div class="flex flex-row gap-2 py-2 px-3 bg-gray-400/30 dark:bg-gray-500/30 rounded-2xl">
 		<ToolBarContent icon={AppsIcon} props={{ size: toolBarIconSize }} />
 		<ToolBarContent icon={SettingsIcon} props={{ size: toolBarIconSize }} />
-		<ToolBarContentDivider {toolBarIconSize} />
-		<ToolBarContent icon={OpenDocumentIcon} props={{ size: toolBarIconSize }} />
-		<ToolBarContent icon={OpenFolderIcon} props={{ size: toolBarIconSize }} />
+		{#if hasOpenFolder || hasOpenFile}
+			<ToolBarContentDivider {toolBarIconSize} />
+		{/if}
+		{#if hasOpenFile}
+			<ToolBarContent icon={OpenDocumentIcon} props={{ size: toolBarIconSize }} />
+		{/if}
+		{#if hasOpenFolder}
+			<ToolBarContent icon={OpenFolderIcon} props={{ size: toolBarIconSize }} />
+		{/if}
 	</div>
 </div>
