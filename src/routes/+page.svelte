ac1f0245a031d3c9c5d05f6766323fa22f2f3d63
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-09 23:10:29 +0600

    Update folder organization in landing and landing content max width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,11 @@
 	const webWindowName: string = 'root';
 
 	onMount(() => {
-		openWindow('root', rootFolder, 150);
+		openWindow('root', rootFolder, 120);
 	});
 </script>
 
-<div class="max-w-[1300px] h-full mx-auto flex flex-col justify-center">
+<div class="max-w-[1000px] h-full mx-auto flex flex-col justify-center">
 	<div class="flex flex-col md:flex-row justify-between gap-10">
 		<div class="md:w-2/5 md:pl-10 flex flex-col justify-center gap-10">
 			<Logo
@@ -29,7 +29,7 @@
 				<h1 class="text-[2rem] text-center leading-8">{aboutMe.person.first}'s Portfolio</h1>
 			</div>
 		</div>
-		<div class="md:w-3/5 flex flex-col justify-center gap-20 px-5 md:pl-0 md:pr-10">
+		<div class="max-w-[340px] mx-auto flex flex-col justify-center gap-20 px-5 md:pl-0 md:pr-10">
 			<DirectorySystemComponent {webWindowName} />
 		</div>
 	</div>
