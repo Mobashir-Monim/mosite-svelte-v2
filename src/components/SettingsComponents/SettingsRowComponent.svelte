3a0335d7baaffc65bf32af508fd25d0386f3e60c
Mobashir Monim <mobashirmonim@gmail.com>
2023-09-25 06:07:06 +0600

    Fix issue with settings text alignment

--- src/components/SettingsComponents/SettingsRowComponent.svelte
+++ src/components/SettingsComponents/SettingsRowComponent.svelte
@@ -6,7 +6,7 @@
 <div
 	class="flex flex-col md:flex-row w-full md:justify-between border-[0.5px] p-3 rounded-2xl border-surface-500 dark:border-white/70 bg-surface-200 dark:bg-white/10"
 >
-	<div class="md:w-[40%] my-auto text-[0.7rem] mx-auto md:ml-0 md:mr-auto">
+	<div class="md:w-[40%] my-auto text-[0.7rem] mx-auto md:ml-0 md:mr-auto text-left">
 		{prompt}
 	</div>
 	<div class="mx-auto md:ml-auto md:mr-0">
