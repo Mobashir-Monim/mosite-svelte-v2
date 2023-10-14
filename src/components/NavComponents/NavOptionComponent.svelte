24024ff4bc7f288cd3f885456bd5fcb56fcb1672
Mobashir Monim <mobashirmonim@gmail.com>
2023-10-14 15:59:45 +0600

    Update nav option component

--- src/components/NavComponents/NavOptionComponent.svelte
+++ src/components/NavComponents/NavOptionComponent.svelte
@@ -1,58 +1,21 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { NavOptionType } from '$lib/types';
+	import NavOptionContent from './NavOptionContent.svelte';
 
 	export let option: NavOptionType;
 	export let expanded: boolean = false;
+
+	$: classes = `flex flex-row ${
+		expanded ? 'gap-3' : 'gap-0'
+	} text-white hover:text-primary-400 transit group justify-left ${option.classes ?? ''}`;
 </script>
 
 {#if option.url}
-	<a
-		href={option.url}
-		class="flex flex-row {expanded
-			? 'gap-3'
-			: 'gap-0'} text-white hover:text-primary-400 transit group justify-left {option.classes ??
-			''}"
-	>
-		<div class="p-2 bg-white/20 rounded-xl transit group-hover:bg-primary-400">
-			<svelte:component
-				this={option.icon}
-				size={25}
-				classes="fill-white group-hover:fill-tertiary-400"
-			/>
-		</div>
-
-		<div
-			class="inline-block my-auto {expanded
-				? 'w-full text-[0.8rem] text-opacity-100'
-				: 'w-[0%] text-[0rem] text-opacity-0'} transit overflow-hidden"
-		>
-			{option.name}
-		</div>
+	<a href={option.url} class={classes}>
+		<NavOptionContent {option} {expanded} />
 	</a>
 {:else if option.callback}
-	<button
-		type="button"
-		on:click={option.callback}
-		class="flex flex-row {expanded
-			? 'gap-3'
-			: 'gap-0'} text-white hover:text-primary-400 transit group justify-left {option.classes ??
-			''}"
-	>
-		<div class="p-2 bg-white/20 rounded-xl transit group-hover:bg-primary-400">
-			<svelte:component
-				this={option.icon}
-				size={25}
-				classes="fill-white group-hover:fill-tertiary-400"
-			/>
-		</div>
-
-		<div
-			class="inline-block my-auto {expanded
-				? 'w-full text-[0.8rem] text-opacity-100'
-				: 'w-[0%] text-[0rem] text-opacity-0'} transit overflow-hidden"
-		>
-			{option.name}
-		</div>
+	<button type="button" on:click={option.callback} class={classes}>
+		<NavOptionContent {option} {expanded} />
 	</button>
 {/if}
---
+++ src/components/NavComponents/NavOptionContent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { NavOptionType } from '$lib/types';
+
+	export let option: NavOptionType;
+	export let expanded: boolean = false;
+</script>
+
+<div class="p-2 bg-white/20 rounded-xl transit group-hover:bg-primary-400">
+	<svelte:component
+		this={option.icon}
+		size={25}
+		classes="fill-white group-hover:fill-tertiary-400"
+	/>
+</div>
+
+<div
+	class="inline-block my-auto {expanded
+		? 'w-full text-[0.8rem] text-opacity-100'
+		: 'w-[0%] text-[0rem] text-opacity-0'} transit overflow-hidden"
+>
+	{option.name}
+</div>
